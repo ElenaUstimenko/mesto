46506edf384b915a26a83861bdfc34f2ecccdc81
Elena Ustimenko <ystimenko.e@mail.ru>
2023-02-13 23:03:21 +0300

    13.02.2023

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,44 @@ const aditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__text_type_name');
 let jobInput = document.querySelector('.popup__text_type_profession');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
 
-
 const handleAditButtonClick = () => {
-  popup.classList.add('popup_opened');
+  openPopup();
 }
 
 const handleCloseButtonClick = () => {
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
-aditButton.addEventListener('click', handleAditButtonClick);
-closeButton.addEventListener('click', handleCloseButtonClick);
+const handleOverlayClick = (event) => {
+  if (event.target === event.currentTarget){
+    closePopup();
+  } 
+}
 
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileText.textContent;
+}
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileText.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
+aditButton.addEventListener('click', handleAditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
+popup.addEventListener('click', handleOverlayClick)
 formElement.addEventListener('submit', handleFormSubmit);
